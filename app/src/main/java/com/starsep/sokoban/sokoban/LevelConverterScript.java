390f5a6b22c2265d30b0cb59f7b6e883fe24d847
Filip Czaplicki <filipczaplicki@gmail.com>
2016-08-23 00:27:54 +0200

    LevelConverterScript

---
+++ app/src/main/java/com/starsep/sokoban/sokoban/LevelConverterScript.java
@@ -0,0 +1,28 @@
+package com.starsep.sokoban.sokoban;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class LevelConverterScript {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		ArrayList<char[]> data = new ArrayList<>();
+		while (input.hasNextLine()) {
+			String line = input.nextLine();
+			if (line != null) {
+				data.add(line.toCharArray());
+			}
+		}
+		char[][] level = new char[data.size()][];
+		for (int i = 0; i < level.length; i++) {
+			level[i] = new char[data.get(i).length];
+			System.arraycopy(data.get(i), 0, level[i], 0, data.get(i).length);
+		}
+		try {
+			Level l = LevelConverter.convert(level);
+			System.out.print(l);
+		} catch (LevelConverter.LevelConverterException e) {
+			e.printStackTrace();
+		}
+	}
+}
