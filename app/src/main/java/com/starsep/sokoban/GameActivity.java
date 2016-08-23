eb2db4ea903e9fc9b9ea44e488f153a495167697
Filip Czaplicki <filipczaplicki@gmail.com>
2016-08-23 22:58:35 +0200

    Unused imports removed in GameActivity

--- app/src/main/java/com/starsep/sokoban/GameActivity.java
+++ app/src/main/java/com/starsep/sokoban/GameActivity.java
@@ -1,13 +1,9 @@
 package com.starsep.sokoban;
 
 import android.app.Activity;
-import android.content.IntentSender;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v7.view.menu.*;
-import android.util.Log;
-import android.view.View;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
