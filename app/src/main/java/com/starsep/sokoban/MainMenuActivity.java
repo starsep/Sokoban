bb78bea7c5d88ff9b873f80a41f6d20eab8a41b7
Filip Czaplicki <filipczaplicki@gmail.com>
2016-09-10 00:42:23 +0200

    Menu buttons added, function stubs

--- app/src/main/java/com/starsep/sokoban/MainMenuActivity.java
+++ app/src/main/java/com/starsep/sokoban/MainMenuActivity.java
@@ -1,6 +1,7 @@
 package com.starsep.sokoban;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -13,8 +14,21 @@ public class MainMenuActivity extends Activity {
 		setContentView(R.layout.activity_main_menu);
 	}
 
-	public void startButtonClicked(View view) {
+	public void newGameButtonClicked(View view) {
 		Intent intent = new Intent(this, GameActivity.class);
 		startActivity(intent);
+	}
+
+	public void helpButtonClicked(View view) {
+		// Intent intent = new Intent(this, HelpActivity.class);
+		// startActivity(intent);
+	}
+
+	public void settingsButtonClicked(View view) {
+		// Dialog settingsDialog = SettingsDialog.build(getApplicationContext());
+		// settingsDialog.show();
+	}
+	
+	public void exitButtonClicked(View view) {
 	}
 }
