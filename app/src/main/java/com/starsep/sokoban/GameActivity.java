792dee2fb605f2239cbce08413f60ec2224fdf7d
Filip Czaplicki <filipczaplicki@gmail.com>
2016-09-11 02:14:41 +0200

    Google_play_games code removed from master

--- app/src/main/java/com/starsep/sokoban/GameActivity.java
+++ app/src/main/java/com/starsep/sokoban/GameActivity.java
@@ -16,11 +16,7 @@ import com.starsep.sokoban.gamelogic.HighScore;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class GameActivity extends Activity implements
-		GoogleApiClient.ConnectionCallbacks,
-		GoogleApiClient.OnConnectionFailedListener,
-		GameController {
-	// private GoogleApiClient googleApiClient;
+public class GameActivity extends Activity implements GameController {
 	private GameView gameView;
 	private GameModel gameModel;
 	private TextView statusTextView;
@@ -29,13 +25,11 @@ public class GameActivity extends Activity implements
 	@Override
 	protected void onStop() {
 		super.onStop();
-		// googleApiClient.disconnect();
 	}
 
 	@Override
 	protected void onStart() {
 		super.onStart();
-		// googleApiClient.connect();
 	}
 
 	@Override
@@ -77,41 +71,8 @@ public class GameActivity extends Activity implements
 				runOnUiThread(() -> gameModel.onSecondElapsed());
 			}
 		}, 0, 1000);
-		// googleApiClient = AchievementClient.build(this, gameView);
 	}
 
-	@Override
-	public void onConnected(@Nullable Bundle bundle) {
-		// Log.d(TAG, "onConnected(): connected to Google APIs");
-		// Games.setViewForPopups(googleApiClient, getWindow().getDecorView().findViewById(android.R.id.content));
-		// onAchievementUnlock(R.string.achievement_leet);
-	}
-
-	@Override
-	public void onConnectionSuspended(int i) {
-
-	}
-
-	@Override
-	public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-		// Log.d(TAG, "onConnectionFailed(): attempting to resolve" + connectionResult);
-		// try {
-		//      connectionResult.startResolutionForResult(this, 9143);
-		// } catch (IntentSender.SendIntentException e) {
-		//      e.printStackTrace();
-		// }
-	}
-
-	/*@Override
-	public void onAchievementUnlock(int id) {
-		 Log.d(TAG, "Trying to unlock achievement");
-		 googleApiClient.connect();
-		 if (googleApiClient != null && googleApiClient.isConnected()) {
-				Log.d(TAG, getResources().getString(id));
-				Games.Achievements.increment(googleApiClient, getResources().getString(id), 300);
-		      startActivityForResult(Games.Achievements.getAchievementsIntent(googleApiClient), 9111);
-		 }
-	}*/
 
 	public void resetButtonClicked(View view) {
 		gameModel.repeatLevel();
